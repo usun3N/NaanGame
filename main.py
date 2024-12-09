adfcbc7da2feaee4c4a1e052439d62262398a83f
usun3N <C0A2302260@edu.teu.ac.jp>
2024-12-09 14:22:03 +0900

    add button

--- main.py
+++ main.py
@@ -9,6 +9,77 @@ class Tools:
     def text_surface(text: str, size: int, color: tuple[int, int, int]) -> pg.Surface:
         font = pg.font.Font(None, size)
         return font.render(text, True, color)
+
+    def shift_color(color: tuple[int, int, int], shift: int) -> tuple[int, int, int]:
+        color = [i + shift if i + shift <= 255 else 255 for i in color]
+        return color
+
+    
+class Clickable:
+    def __init__(self, coordinate: tuple[int, int], size: tuple[int, int]) -> None:
+        self.x, self.y = coordinate
+        self.width, self.height = size
+        self.hover = 0
+        self.left = 0
+        self.middle = 0
+        self.right = 0
+
+    def update(self, screen: pg.Surface):
+        mouse_pos = pg.mouse.get_pos()
+        left, middle, right = pg.mouse.get_pressed()
+        if self.x < mouse_pos[0] < self.x + self.width and self.y < mouse_pos[1] < self.y + self.height:
+            self.left += left
+            self.middle += middle
+            self.right += right
+            self.hover += 1
+        else:
+            self.hover = 0
+        if not left: self.left = 0
+        if not middle: self.middle = 0
+        if not right: self.right = 0
+        self.render(screen)
+    
+    def render(self, screen: pg.Surface):
+        pass
+    
+    def get_hover(self) -> bool:
+        return self.hover > 0
+
+    def get_clicked(self, button: int=0) -> tuple[bool, bool, bool]:
+        res = (self.left != 0, self.middle != 0, self.right != 0)
+        return res[button]
+    
+    def get_hold(self, frame: int, button: int=0) -> tuple[bool, bool, bool]:
+        res = (self.left > frame, self.middle > frame, self.right > frame)
+        return res[button]
+    
+    def get_clicked_once(self, button: int=0) -> tuple[bool, bool, bool]:
+        res = (self.left == 1, self.middle == 1, self.right == 1)
+        return res[button]
+
+        
+
+
+
+    
+class Button(Clickable):
+    def __init__(self, text: str, coordinate: tuple[int, int], size: tuple[int, int], color: tuple[int, int, int]) -> None:
+        super().__init__(coordinate, size)
+        self.text = text
+        self.x, self.y = coordinate
+        self.width, self.height = size
+        self.color = color
+        self.text_surface = Tools.text_surface(self.text, 30, (0, 0, 0))
+    
+    def render(self, screen:pg.Surface):
+        surface = pg.Surface((self.width, self.height))
+        if self.get_hover():
+            color = Tools.shift_color(self.color, 50)
+        else:
+            color = self.color
+        surface.fill(color)
+        surface.blit(self.text_surface, (self.width / 2 - self.text_surface.get_width() / 2, self.height / 2 - self.text_surface.get_height() / 2))
+        screen.blit(surface, (self.x, self.y))
     
 
 class Main:
@@ -16,7 +87,8 @@ class Main:
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
         self.running = True
-        self.scene = MainMenu(self)
+        self.scene = None
+        self.change_scene(MainMenu(self))
 
     def run(self):
         while self.running:
@@ -28,6 +100,9 @@ class Main:
             self.scene.update(events)
             self.clock.tick(60)
             pg.display.flip()
+    
+    def change_scene(self, scene):
+        self.scene = scene
             
 
 class Scene:
@@ -35,11 +110,15 @@ class Scene:
         self.main = main
         self.event_block = True
         self.overlays = []
+        self.setup()
+    
+    def setup(self):
+        pass
 
     def update(self, events: list[pg.event.Event]):
         self.process()
         for overlay in self.overlays:
-            overlay_surface =overlay.update()
+            overlay_surface = overlay.process()
             self.main.screen.blit(overlay_surface, (0, 0))
 
         for overlay in reversed(self.overlays):
@@ -60,13 +139,13 @@ class Overlay:
     def __init__(self, root_scene: Scene):
         self.root_scene = root_scene
         self.event_block = True
+        self.setup()
     
-    def update(self) -> pg.Surface:
-        """
-        オーバーレイの更新メソッド
-        Returns:
-            pg.Surface: オーバーレイのSurface
-        """
+    def setup(self):
+        pass
+    
+    def proecss(self) -> pg.Surface:
+        pass
 
     def event(self, event_list) -> bool: # Trueで以降のイベントをブロック
         pass
@@ -76,12 +155,26 @@ class Overlay:
 
 
 class MainMenu(Scene):
+    def setup(self):
+        self.buttons = {}
+        self.buttons["start"] = Button("Start", (100, 200), (100, 50),(100, 100, 100))
+        self.buttons["settings"] = Button("Settings", (100, 300), (100, 50),(100, 100, 100))
+        self.buttons["exit"] = Button("Exit", (100, 400), (100, 50),(100, 100, 100))
+
     def process(self):
         screen = self.main.screen
         screen.fill((255, 255, 255))
         screen.blit(Tools.text_surface("Main Menu", 100, (0, 0, 0)), (100, 100))
+        for button in self.buttons.values():
+            button.update(screen)
     
     def event(self, event_list: list[pg.event.Event]):
+        if self.buttons["start"].get_clicked_once():
+            self.main.change_scene(CharacterSelect(self.main))
+        if self.buttons["settings"].get_clicked_once():
+            self.overlays.append(SettingMenu(self))
+        if self.buttons["exit"].get_clicked_once():
+            self.main.running = False
         for event in event_list:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
@@ -90,16 +183,13 @@ class MainMenu(Scene):
                     print("aaa")
 
 class PauseMenu(Overlay):
-    def update(self):
-        screen = pg.Surface((WIDTH, HEIGHT))
-        bg = pg.Surface((WIDTH, HEIGHT))
-        bg.fill((0, 0, 0))
-        bg.set_alpha(128)
-        screen.blit(bg, (0, 0))
+    def process(self):
+        screen = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA)
+        screen.fill((0, 0, 0, 200))
 
         text = Tools.text_surface("Pause Menu", 100, (255, 255, 255))
-        screen.blit(text, (100, 100))
-        return bg
+        screen.blit(text, (50, 50))
+        return screen
     
     def event(self, event_list):
         for event in event_list:
@@ -109,6 +199,32 @@ class PauseMenu(Overlay):
                 if event.key == pg.K_a:
                     print("overlay")
         return self.event_block
+
+class SettingMenu(Overlay):
+    def setup(self):
+        self.buttons = {}
+        self.buttons["back"] = Button("Back", (600, 500), (100, 50), (100, 100, 100))
+        self.buttons["pause"] = Button("Pause", (100, 200), (100, 50), (100, 100, 100))
+
+    def process(self):
+        screen = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA)
+        screen.fill((0, 0, 0, 200))
+
+        text = Tools.text_surface("Settings", 100, (255, 255, 255))
+        screen.blit(text, (50, 50))
+        for button in self.buttons.values():
+            button.update(screen)
+        return screen
+    
+    def event(self, event_list):
+        if self.buttons["back"].get_clicked_once():
+            self.close()
+        return self.event_block
+
+class CharacterSelect(Scene):
+    def process(self):
+        screen = pg.Surface((WIDTH, HEIGHT))
+        screen.fill((255, 255, 255))
 
 
 if __name__ == "__main__":
