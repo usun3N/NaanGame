9c4b9abf19b3b8192a59239f32c53d6ded639a06
usun3N <C0A2302260@edu.teu.ac.jp>
2024-12-09 16:56:59 +0900

    add Ingame Scene

--- main.py
+++ main.py
@@ -23,20 +23,26 @@ class Clickable:
         self.left = 0
         self.middle = 0
         self.right = 0
+        self.click_protect = True
 
     def update(self, screen: pg.Surface):
         mouse_pos = pg.mouse.get_pos()
         left, middle, right = pg.mouse.get_pressed()
-        if self.x < mouse_pos[0] < self.x + self.width and self.y < mouse_pos[1] < self.y + self.height:
-            self.left += left
-            self.middle += middle
-            self.right += right
-            self.hover += 1
-        else:
-            self.hover = 0
-        if not left: self.left = 0
-        if not middle: self.middle = 0
-        if not right: self.right = 0
+        
+        if self.click_protect:
+            if not (left or middle or right):
+                self.click_protect = False
+        if not self.click_protect:
+            if self.x < mouse_pos[0] < self.x + self.width and self.y < mouse_pos[1] < self.y + self.height:
+                self.left += left
+                self.middle += middle
+                self.right += right
+                self.hover += 1
+            else:
+                self.hover = 0
+            if not left: self.left = 0
+            if not middle: self.middle = 0
+            if not right: self.right = 0
         self.render(screen)
     
     def render(self, screen: pg.Surface):
@@ -89,6 +95,7 @@ class Main:
         self.running = True
         self.scene = None
         self.change_scene(MainMenu(self))
+        self.controll_manager = ControllManager()
 
     def run(self):
         while self.running:
@@ -222,10 +229,151 @@ class SettingMenu(Overlay):
         return self.event_block
 
 class CharacterSelect(Scene):
+    def setup(self):
+        self.buttons = {}
+        self.buttons["back"] = Button("Back", (600, 500), (100, 50), (100, 100, 100))
+        self.buttons["start"] = Button("Start", (100, 200), (100, 50), (100, 100, 100))
     def process(self):
-        screen = pg.Surface((WIDTH, HEIGHT))
+        screen = self.main.screen
         screen.fill((255, 255, 255))
+        text = Tools.text_surface("Character Select", 100, (0, 0, 0))
+        for button in self.buttons.values():
+            button.update(screen)
+        screen.blit(text, (50, 50))
 
+    def event(self, event_list):
+        if self.buttons["back"].get_clicked_once():
+            self.main.change_scene(MainMenu(self.main))
+        if self.buttons["start"].get_clicked_once():
+            self.main.change_scene(Ingame(self.main))
+
+class Ingame(Scene):
+    def setup(self):
+        self.player = Player()
+        self.enemies = pg.sprite.Group()
+        self.projectiles = pg.sprite.Group()
+        self.items = []
+
+    def process(self):
+        screen = self.main.screen
+        screen.fill((255, 255, 255))
+        text = Tools.text_surface(f"{self.player.stamina}", 30, (0, 0, 0))
+        screen.blit(text, (50, 50))
+        text = Tools.text_surface(f"{self.player.stamina_cooldown}", 30, (0, 0, 0))
+        screen.blit(text, (50, 100))
+        self.player.update()
+        self.player.draw(screen)
+    
+    def event(self, event_list: list[pg.event.Event]):
+        actions = self.main.controll_manager.update(event_list)
+        speed = 2
+        if actions["dash"] > 0 and self.player.stamina > 0:
+            speed = 7
+            self.player.decrease_stamina(1)
+        if actions["move_left"] > 0:
+            self.player.move((-speed, 0))
+        if actions["move_right"] > 0:
+            self.player.move((speed, 0))
+        if actions["move_up"] > 0:
+            self.player.move((0, -speed))
+        if actions["move_down"] > 0:
+            self.player.move((0, speed))
+        if actions["pause"] > 0:
+            self.overlays.append(PauseMenu(self))
+            actions["pause"] = 0
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.hp = 100
+        self.max_hp = 100
+        self.stamina = 50
+        self.max_stamina = 50
+        self.stamina_cooldown = 0
+        self.stamina_decreasing = False
+        self.stamina_regen = False
+        self.image = pg.image.load("discord.png")
+        self.rect = self.image.get_rect()
+
+    def decrease_stamina(self, amount: int):
+        if self.stamina - amount < 0:
+            self.stamina = 0
+        else:
+            self.stamina -= amount
+        self.stamina_cooldown += 1
+        self.stamina_decreasing = True
+    
+    def increase_stamina(self, amount: int):
+        if self.stamina + amount > self.max_stamina:
+            self.stamina = self.max_stamina
+        else:
+            self.stamina += amount
+
+    def move(self, direction: tuple[int, int]):
+        vx, vy = direction
+        self.x += vx
+        self.y += vy
+    
+    def update(self):
+        if self.stamina_cooldown > 0:
+            if not self.stamina_decreasing:
+                self.stamina_cooldown -= 1
+        else:
+            self.stamina_regen = True
+        if self.stamina_regen:
+            self.increase_stamina(1)
+        self.stamina_decreasing = False
+
+    def draw(self, screen: pg.Surface):
+        screen.blit(self.image, (self.x, self.y))
+        
+
+        
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.hp = 20
+    
+class ControllManager:
+    def __init__(self):
+        self.key_map = {
+            "move_left": pg.K_a,
+            "move_right": pg.K_d,
+            "move_up": pg.K_w,
+            "move_down": pg.K_s,
+            "dash": pg.K_LSHIFT,
+            "pause": pg.K_p
+        }
+        self.key_hold = {
+            "move_left": 0,
+            "move_right": 0,
+            "move_up": 0,
+            "move_down": 0,
+            "dash": 0,
+            "pause": 0
+        }
+
+    def set_key(self, key, action):
+        self.key_map[action] = key
+
+    def update(self, events: list[pg.event.Event]):
+        actions = []
+        for event in events:
+            if event.type == pg.KEYDOWN:
+                for action, key in self.key_map.items():
+                    if event.key == key:
+                        self.key_hold[action] += 1
+            if event.type == pg.KEYUP:
+                for action, key in self.key_map.items():
+                    if event.key == key:
+                        self.key_hold[action] = 0
+        return self.key_hold
+
+                
 
 if __name__ == "__main__":
     main = Main()
